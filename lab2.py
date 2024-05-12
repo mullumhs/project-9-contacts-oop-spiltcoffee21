4492e46cb34353b5e8898f98d5f77174bfb66d8e
spiltcoffee21 <140124277+spiltcoffee21@users.noreply.github.com>
2024-05-12 23:50:11 +0000

    fghdgsdegtder

--- lab2.py
+++ lab2.py
@@ -10,10 +10,23 @@
 # Create a class named Contact that represents a contact in a contact management system. 
 # This class should have an initialiser with attributes for name, phone_number, and email.
 # Add a class attribute to keep track of the total number of contacts.
+class Contact:
+    number_of_contact = 0
+    
+    def __init__(self, name, phone_number, email):
+        self.name = name
+        self.phone_number = phone_number
+        self.email = email
+        
+        
+    
 
 
 
 # Create at least two instances of the Contact class with different data.
+contact1 = Contact("remy", "04568239454", "")
+contact2 = Contact("", "E456")
+
 
 
 
